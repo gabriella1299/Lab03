a8ecaa7d3cd286557d4b6b3095ba448a06b45f2f
nagab <nagab@192.168.1.46>
2021-03-24 09:26:12 +0100

    Creazione interfaccia con SceneBuilder

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -6,20 +6,74 @@ package it.polito.tdp.spellchecker;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private MenuButton MenuBtn;
+
+    @FXML
+    private TextArea TxtFrase;
+
+    @FXML
+    private Button BtnCheck;
+
+    @FXML
+    private TextArea TxtParoleSbagliate;
+
+    @FXML
+    private Label LblErrori;
+
+    @FXML
+    private Button TxtClear;
+
+    @FXML
+    private Label LblTempo;
+
+    @FXML
+    void doCheck(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doEnglish(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doItalian(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert MenuBtn != null : "fx:id=\"MenuBtn\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert TxtFrase != null : "fx:id=\"TxtFrase\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert BtnCheck != null : "fx:id=\"BtnCheck\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert TxtParoleSbagliate != null : "fx:id=\"TxtParoleSbagliate\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert LblErrori != null : "fx:id=\"LblErrori\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert TxtClear != null : "fx:id=\"TxtClear\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert LblTempo != null : "fx:id=\"LblTempo\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
+
 
 
